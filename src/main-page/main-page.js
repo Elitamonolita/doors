20d563a6c86cf4e957fd85e4690393230f0ca3a8
serg-bs <serg-bs@yandex.ru>
2022-08-30 18:12:58 +0300

    2

--- src/main-page/main-page.js
+++ src/main-page/main-page.js
@@ -1,17 +1,12 @@
 import React from "react";
 import "./main-page.css";
 import Menu from "../menu/menu";
-import MainImg from "./main-img/main-img";
-import PageInfo from "./page-info/page-info";
-import Photo from "./photo/photo";
+
 
 const MainPage = () => {
     return (
         <div className="MainBody">
         <Menu/>
-            <MainImg/>
-            <Photo/>
-            <PageInfo/>
         </div>
     )
 }
--- src/menu/menu.js
+++ src/menu/menu.js
@@ -1,31 +1,15 @@
 import React from "react";
 import "./menu.css"
-import phone from "../img/phone.png";
+import img from "../img/phone.png";
 
 const Menu = ()=> {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light ">
-                <div className="container-fluid menuPadding">
-                    <div id="navbarColor03">
-                        <ul className="navbar-nav me-auto">
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Двери</a>
-                            </li>
-                                <li className="nav-item">
-                                <a className="nav-link" href="#">Окна</a>
-                                </li>
-                                    <li className="nav-item">
-                                <a className="nav-link" href="#">Остальное</a>
-                            </li>
-                            <img className="phone" src={phone}/>
-                            <div className="phoneNumber">
-                                <p>Номер:</p><p className="number">+7853525325</p>
-                            </div>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+            <div className="head-block">
+                <h3 className="name">Двери Окна Оптим</h3>
+                <div className="name-rf">"Двериокна.рф"</div>
+                <img src={img} className="logo-number"></img>
+            </div>
         </div>
     )
 }
